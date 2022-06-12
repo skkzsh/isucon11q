6541a715917a1ea96f026b0a8322927c8be0f11b
skkzsh <skkzsh12@gmail.com>
2022-06-13 00:44:11 +0900

    Write bench script

---
+++ tool/restart-and-bench.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euvx
+
+sudo truncate -s 0 -c /var/log/nginx/access.log
+sudo truncate -s 0 -c /var/log/mysql/slow.log
+# mysqladmin flush-logs
+
+sudo systemctl restart isucondition.go
+sudo systemctl restart jiaapi-mock
+sudo systemctl restart mysql
+sudo systemctl restart nginx
+
+cd ~/bench
+./bench -all-addresses 127.0.0.11 -target 127.0.0.11:443 -tls -jia-service-url http://127.0.0.1:4999
+
+sudo cat /var/log/nginx/access.log | \
+	alp json -m '^/api/condition/[0-9a-f\-]+$','^/api/isu/[0-9a-f\-]+$','^/api/isu/[0-9a-f\-]+/graph$','^/api/isu/[0-9a-f\-]+/icon$','^/isu/[0-9a-f\-]+$','^/isu/[0-9a-f\-]+/condition$','^/isu/[0-9a-f\-]+/graph$' --sort avg -r
+
