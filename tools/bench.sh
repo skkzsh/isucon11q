4e296f7ed80ddb9532b309dc7ad15ceb73a11efc
skkzsh <skkzsh12@gmail.com>
2022-07-22 17:59:06 +0900

    Set slow query log

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,7 +3,7 @@ set -euvx
 
 sudo truncate -s 0 -c /var/log/nginx/access.log
 sudo truncate -s 0 -c /var/log/nginx/error.log
-sudo truncate -s 0 -c /var/log/mysql/mysql-slow.log
+sudo truncate -s 0 -c /var/log/mysql/mariadb-slow.log
 sudo truncate -s 0 -c /var/log/mysql/error.log
 # mysqladmin flush-logs
 
@@ -26,8 +26,8 @@ sudo cat /var/log/nginx/access.log |
 	tee ~/logs/alp-$(date +%Y%m%d-%H%M%S).log
 # -q --qs-ignore-values 
 
-# sudo mysqldumpslow /var/log/mysql/mysql-slow.log | tee ~/logs/slow-$(date +%Y%m%d-%H%M%S).log
-# sudo pt-query-digest /var/log/mysql/mysql-slow.log | tee ~/logs/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
+# sudo mysqldumpslow /var/log/mysql/mariadb-slow.log | tee ~/logs/slow-$(date +%Y%m%d-%H%M%S).log
+# sudo pt-query-digest /var/log/mysql/mariadb-slow.log | tee ~/logs/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
 
 # go tool pprof -http=:10060 http://localhost:6060/debug/pprof/profile
 
