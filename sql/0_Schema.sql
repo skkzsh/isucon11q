38eb45ff3c121ec4cb84c0d0d61a19c1609911eb
skkzsh <skkzsh12@gmail.com>
2024-11-09 09:36:24 +0900

    Solve slow query #1

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -26,6 +26,8 @@ CREATE TABLE `isu_condition` (
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+CREATE INDEX idx_jia_isu_uuid_timestamp ON isu_condition (jia_isu_uuid, timestamp);
+
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
