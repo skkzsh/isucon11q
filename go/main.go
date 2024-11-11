05e5273916956501a04aeee064016c32d51bb8c6
skkzsh <skkzsh12@gmail.com>
2024-11-12 01:22:42 +0900

    db error: sql: Scan error on column index 0, name \"character\": converting NULL to string is unsupported" #1

--- go/main.go
+++ go/main.go
@@ -1160,7 +1160,8 @@ func getIsuConditionsFromDB(db *sqlx.DB, jiaIsuUUID string, endTime time.Time, c
 // ISUの性格毎の最新のコンディション情報
 func getTrend(c echo.Context) error {
 	characterList := []Isu{}
-	err := db.Select(&characterList, "SELECT `character` FROM `isu` GROUP BY `character`")
+	// err := db.Select(&characterList, "SELECT `character` FROM `isu` GROUP BY `character`")
+	err := db.Select(&characterList, "SELECT `character` FROM `isu` GROUP BY `character` having `character` is not null")
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
